87534e51856cb47d2e86ebf0a9fbf2e4508aa61e
yuvrajraghuvanshi <yuvrajraghuvanshi070101@gmail.com>
2024-01-03 18:49:52 +0530

    friends-api added

--- controllers/followControllers/index.js
+++ controllers/followControllers/index.js
@@ -46,4 +46,24 @@ const unfollowController=async(req,res)=>{
    }
 }
 
-module.exports={followController,unfollowController}
+const getFollowers=async(req,res)=>{
+     try{
+         const user= await User.findById(req.params.userId);
+       const friends=await Promise.all(
+        user.followings.map(friendId=>{
+            return User.findById(friendId)
+        })
+       )
+       let friendList=[];
+       friends.map(friend=>{
+        const {_id,username,profilePicture}=friend;
+        friendList.push({_id,username,profilePicture})
+       })
+       res.status(200).json(friendList)
+     }
+     catch(err){
+         res.status(500).json(err)
+     }
+    
+ }
+module.exports={followController,unfollowController,getFollowers}
--- routes/followRoute/index.js
+++ routes/followRoute/index.js
@@ -1,7 +1,8 @@
-const { followController, unfollowController } = require('../../controllers/followControllers');
+const { followController, unfollowController, getFollowers } = require('../../controllers/followControllers');
 
 const router=require('express').Router();
 
 router.put('/:id/follow',followController)
 router.put('/:id/unfollow',unfollowController)
+router.get('/friends/:userId',getFollowers)
 module.exports=router;
